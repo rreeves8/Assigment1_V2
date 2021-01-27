3c088b33f5c9534dfbab4c7d8a7fa977a21a34ed
rreeves8 <rreeves8@uwo.ca>
2021-01-27 16:59:38 -0500

    Version1
    missing dynamic pickups, restart functionality, custom map, and broken camera

---
+++ Assignment1_V2/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Target, Player;
+
+    public float rotateSpeed = 1;
+
+    private float mouseX, mouseY;
+
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    void LateUpdate()
+    {
+        mouseX += (Input.GetAxis("Mouse X")  * rotateSpeed);
+        mouseY -= (Input.GetAxis("Mouse Y") * rotateSpeed);
+
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+
+    }
+}
---
+++ Assignment1_V2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour{
+
+    public float speed;
+
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        countText.text = "Count: " + count.ToString();
+
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "Count: " + count.ToString();
+        }
+    }
+
+  
+}
